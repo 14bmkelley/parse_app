8bb52a1845507451024763bf08d8e8348970dd77
Brandon Kelley <14bmkelley@gmail.com>
2015-05-13 13:59:40 -0700

    Basic layout complete. Listener for activity transition started.

--- src/main/java/com/brandonmkelley/parse_app/LoginActivity.java
+++ src/main/java/com/brandonmkelley/parse_app/LoginActivity.java
@@ -1,14 +1,36 @@
 package com.brandonmkelley.parse_app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+
+	private Button submit = (Button) findViewById(R.id.login_submit);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		setListeners();
+	}
+
+	private void setListeners() {
+
+		submit.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				Intent intent = new Intent(this, internal.class);
+				startActivity(intent);
+
+			}
+
+		});
+
 	}
 
 }
